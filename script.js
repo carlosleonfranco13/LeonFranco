631b3b3bf0ed8693cd6a510599b6536fdf753459
carlos <carlosleonfranco13@gmail.com>
2023-03-19 18:00:38 -0500

    Mostrar alerta en consola del error de mi form vacío

--- script.js
+++ script.js
@@ -6,4 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
   const inputEmail = document.querySelector('#email');
   const inputMsj = document.querySelector('#mensaje');  
 
+
+  // Asignar eventos
+  inputName.addEventListener('blur', validar);
+
+  inputEmail.addEventListener('blur', validar);
+
+  inputMsj.addEventListener('blur', validar);
+
+  function validar(e) {
+    if(e.target.value.trim() === '') {
+      mostrarAlerta();
+    }else{
+      console.log('Continúa con el formulario');
+    }
+  }
+
+  function mostrarAlerta() {
+    const alertaError = document.createElement('P');
+    alertaError.textContent = 'Error! Form vacío';
+    console.log(alertaError);
+  }
 });
