4130007041b5258a733539e38213a8c01280a229
carlos <carlosleonfranco13@gmail.com>
2023-03-19 17:27:50 -0500

    Evolución de mi Form

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-/*===========CARDS LINKS==========*/
-var check = document.querySelector('input[type="checkbox"].languCheck');
-check.addEventListener('change', cambiarIdioma);
-
-function cambiarIdioma() {
-  let id = check.checked;
-  if (id == true) {
-    location.href = "/en/index.html"; // redirecciona al index.html en la carpeta "en"
-  } else {
-    location.href = "/index.html"; // redirecciona al index.html principal
-  }
-}
-
 /*============FORM==============*/
 document.addEventListener('DOMContentLoaded', function() {
 
   // Seleccionar los elementos de la interfaz
   const inputName = document.querySelector('#name');
   const inputEmail = document.querySelector('#email');
-  const inputMsj = document.querySelector('#mensaje');
+  const inputMsj = document.querySelector('#mensaje');  
 
 });
