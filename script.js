b2ba0c79f39c5311ff6652cfc0e7220f8e6b3d78
carlos <carlosleonfranco13@gmail.com>
2023-03-19 13:49:08 -0500

    Selecciono los elementos de la interfaz

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function cambiarIdioma() {
 }
 
 /*============FORM==============*/
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Seleccionar los elementos de la interfaz
+  const inputName = document.querySelector('#name');
+  const inputEmail = document.querySelector('#email');
+  const inputMsj = document.querySelector('#mensaje');
+
+});
