819b70c63b0af2794581288db84f8db9bb9b548d
carlos <carlosleonfranco13@gmail.com>
2023-03-19 18:24:33 -0500

    Agrego clase de estilos para las alertas del formulario

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Seleccionar los elementos de la interfaz
+  const form = document.querySelector('#form');
   const inputName = document.querySelector('#name');
   const inputEmail = document.querySelector('#email');
   const inputMsj = document.querySelector('#mensaje');  
@@ -15,16 +16,21 @@ document.addEventListener('DOMContentLoaded', function() {
   inputMsj.addEventListener('blur', validar);
 
   function validar(e) {
+    console.log(e.target.id)
+
     if(e.target.value.trim() === '') {
-      mostrarAlerta();
+      mostrarAlerta('El campo email es obligatorio');
     }else{
       console.log('Continúa con el formulario');
     }
   }
 
-  function mostrarAlerta() {
+  function mostrarAlerta(mensaje) {
     const alertaError = document.createElement('P');
-    alertaError.textContent = 'Error! Form vacío';
-    console.log(alertaError);
+    alertaError.textContent = mensaje;
+    alertaError.classList.add('alertaError');
+
+    // Mostrar la Alerta en el form del html(cliente)
+    form.appendChild(alertaError);
   }
 });
